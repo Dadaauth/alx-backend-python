e85da297ef18afbabef05fa4571b3c6ae6fcf367
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-05-03 09:05:45 +0100

    ALX: Project: Python - Variable Annotations: Task #7: Added docstrings to function

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,4 +4,5 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """A documentation for this function out of all usefulness."""
     return k, v * v,
