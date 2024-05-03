44627d7fdd7fbd3974ca8c8f1df4b76613430351
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-05-03 08:52:46 +0100

    ALX: Project: Python - Variable Annotations: Task #5: First Attempt

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A module containing useful information"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """A function that sums a list of floats"""
+    return sum(input_list)
