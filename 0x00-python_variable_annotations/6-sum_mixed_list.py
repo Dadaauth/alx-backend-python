07e9b2ef5de7101c9a610e0a5eb65dfe03508cca
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-05-03 08:57:49 +0100

    ALX: Project: Python - Variable Annotations: Task #5: Fix File Code -> Task #6: First Attempt

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A module containing useful stuffs"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    return sum(mxd_lst)
