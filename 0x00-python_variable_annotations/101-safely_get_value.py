b0f9ea9944d99e168c264cb9bdd075f77352db0d
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-05-03 09:45:31 +0100

    ALX: Project: Python - Variable Annotations: Task #11: Second Attempt

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 """A docstring to make this module work"""
 from typing import Mapping, Any, Union, Type
-type NoneType = None
 
 
 def safely_get_value(dct: Mapping, key: Any, default:
-                     Union[Type, NoneType] = None) -> Union[Any, Type]:
+                     Union[Type, None] = None) -> Union[Any, Type]:
     """A docstring to make this function work"""
     if key in dct:
         return dct[key]
