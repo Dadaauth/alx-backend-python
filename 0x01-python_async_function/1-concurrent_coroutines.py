7a9dd8b031e8bb7fd17681b738cc7b73ce730d47
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-05-08 00:51:23 +0100

    ALX: Project: Python - Async: TAsk #1: First Attempt

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This is my module documentation"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int):
+    """This is my coroutine documentation"""
+    return [await asyncio.create_task(wait_random(max_delay))
+            for _ in range(n)]
