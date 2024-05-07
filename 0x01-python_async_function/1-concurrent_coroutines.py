73514a0da4e7240faad70e270ce4f8ebd6adb56b
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-05-08 00:53:29 +0100

    ALX: Project: Python - Async: Task #1: Second Attempt: Added typehint for function (async def wait_n) return value.

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """This is my module documentation"""
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """This is my coroutine documentation"""
     return [await asyncio.create_task(wait_random(max_delay))
             for _ in range(n)]
