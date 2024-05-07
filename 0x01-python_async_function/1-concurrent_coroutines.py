9c0eaca3c3eab26b9d1b13a162d0f8dc1c7aa423
Dada Clement Authority <dadaauthourity23@gmail.com>
2024-05-08 00:57:52 +0100

    ALX: Project: Python - Async: Task #1: Third Attempt: I tried to arrange the return list in ascending order using async programming

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,5 +7,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """This is my coroutine documentation"""
-    return [await asyncio.create_task(wait_random(max_delay))
+    task = [asyncio.create_task(wait_random(max_delay))
             for _ in range(n)]
+    return [await task for task in asyncio.as_completed(task)]
